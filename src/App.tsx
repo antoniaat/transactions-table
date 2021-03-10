70c62c9f15db2db5159f463bda5f519007ed0fe1
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-10 12:17:09 +0200

    Fixed the project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        wudhuwassash
+      </h1>
+    </div>
+  );
+}
+
+export default App;
--- transactions-table/src/App.js
+++
@@ -1,7 +0,0 @@
-const App = () => (
-  <div className="App">
-    <h1>Transactions </h1>
-  </div>
-);
-
-export default App;
