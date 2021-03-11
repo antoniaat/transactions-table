4bb5b2be91ebfdf7accb8c822605d0aa040e1f17
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-11 09:18:42 +0200

    Moved the default transactions data into the constrants

---
+++ src/scripts/constants.ts
@@ -0,0 +1,29 @@
+import { TransactionResponse } from "../types/transaction";
+
+const emptyTransaction = {
+  id: "",
+  account_id: "",
+  amount: 0,
+  currency_code: "",
+  description: "",
+  category: "",
+  gateway: "",
+  extra: {
+    id: "",
+    payer: "",
+    posting_date: "",
+    posting_time: "",
+    closing_balance: 0,
+    payer_information: "",
+    payer_internal_name: "",
+    account_balance_snapshot: 0,
+    details: "",
+  },
+  created_at: "",
+  updated_at: "",
+}
+
+export const defaultTransactionsData: TransactionResponse = {
+  transactions: new Array(10).fill(emptyTransaction),
+};
+
