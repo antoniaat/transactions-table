fe80472d4bc054d4e2e3a737f3e864be2be22a20
Antonia Atanasova <antoniyaata@gmail.com>
2021-03-13 23:22:54 +0200

    Redesign the table sorting indicators

--- src/scripts/components/table/table-cell-desc-sort.tsx
+++ src/scripts/components/table/table-cell-desc-sort.tsx
@@ -1,11 +1,20 @@
+import '../../../styles/components/table/table-cell-desc-sort.scss'
+
 import React from "react";
+import { getClassName } from "../../utils/react-utils";
 
 interface Props {
   isSortedDesc?: boolean;
 }
 
-const TableCellDescSort: React.FC<Props> = ({ isSortedDesc = true }) => (
-  <span>{isSortedDesc ? " 🔽" : " 🔼"}</span>
-);
+const TableCellDescSort: React.FC<Props> = ({ isSortedDesc = true }) => {
+  const combinedClassName = getClassName({
+    "table-cell-desc-sort": true,
+    "table-cell-desc-sort-up": isSortedDesc,
+    "table-cell-desc-sort-down": !isSortedDesc,
+  });
+
+  return <span className={combinedClassName}></span>;
+};
 
 export default TableCellDescSort;
